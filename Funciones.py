ba82d117487e435a0b98fe26b268cc57f30e50f8
Gabriel Hernandez <gabrielemanuel.hdz@gmail.com>
2025-01-10 00:57:23 -0600

    Create Funciones.py
    Las funciones en pytho se definen con la palabra clave " def nombre_funcion(parametro) : "

---
+++ Funciones.py
@@ -0,0 +1,11 @@
+"""                                    Ejercicio
+En el siguiente código se está llamando a la función saludar, pero no se ha definido. 
+Define la función saludar para que imprima "Hola Mundo" en la consola. Respeta las mayúsculas, 
+minúsculas y espacios."""
+
+def saludar():
+    print ("Hola Mundo")
+  
+saludar()
+saludar()
+saludar()
