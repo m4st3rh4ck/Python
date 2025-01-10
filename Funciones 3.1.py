f48725759fb44182bc5a7adc0b12c15bc9091773
Gabriel Hernandez <gabrielemanuel.hdz@gmail.com>
2025-01-10 01:07:07 -0600

    Create Funciones 3.1.py

---
+++ Funciones 3.1.py
@@ -0,0 +1,11 @@
+"""                      Ejercicio
+Crea la función restar que dependa de tres parámetros a, b y c. 
+Esta función al ser llamada debe mostrar el resultado de la resta 
+de los tres valores ingresados."""
+
+def restar(a, b ,c): #a,b,c son numeros
+    print(a - b - c)
+
+restar(10, 5, 3)
+restar(3, 5, 2)
+restar(9, 4, 1)
