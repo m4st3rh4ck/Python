9ec131f66a793f2b0cc5df99b0ef0452166b1ade
Gabriel Hernandez <gabrielemanuel.hdz@gmail.com>
2025-01-10 01:03:56 -0600

    Create Funciones 3.py

---
+++ Funciones 3.py
@@ -0,0 +1,8 @@
+#Ejercicio: Crea la función restar que dependa de dos parámetros y que imprima la resta de ambos.
+
+def restar(num1, num2):
+    print(num1 - num2)
+
+restar(10, 5)
+restar(3, 5)
+restar(9, 4)
